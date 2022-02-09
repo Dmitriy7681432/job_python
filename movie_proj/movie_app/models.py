96fa51f7ae51fbb99f92f79e67560f9a1be336fb
Aleks <dima.aleksin.93@mail.ru>
2022-02-09 22:26:58 +0300

    Прошел урок 5.6

--- movie_proj/movie_app/models.py
+++ movie_proj/movie_app/models.py
@@ -4,10 +4,20 @@ from  django.utils.text import slugify
 
 # Create your models here.
 class Movie(models.Model):
+    EUR ='EUR'
+    USD ='USD'
+    RUB ='RUB'
+    CURRENCY_CHOICES = [
+        (EUR, 'Euro'),
+        (USD, 'Dollars'),
+        (RUB, 'Rubles'),
+    ]
+
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(default=1000000)
+    currency = models.CharField(max_length=3,choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='',null=False)
 
     def save(self, *args, **kwargs):
@@ -19,5 +29,4 @@ class Movie(models.Model):
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
-
 #from movie_app.models import Movie
